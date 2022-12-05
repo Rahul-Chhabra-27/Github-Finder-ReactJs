66e358e7167f91fe9409537c2b6ffe12f7ab4b7a
Rahul Chhabra <chhabrarahul2707@gmail.com>
2022-12-05 14:23:35 +0530

    Navbar component created.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from './components/ui/navbar';
+
+function App() {
+  return (
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
