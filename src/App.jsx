debb4c0a8a2665072445f0b36c92d1dff22a493a
Rahul Chhabra <chhabrarahul2707@gmail.com>
2022-12-06 16:57:34 +0530

    Footer component created and added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from './components/ui/navbar';
+import Footer from './components/ui/footer';
 
 function App() {
   return (
     <div className="flex flex-col justify-between h-screen">
       <Navbar />
+      <main>Main</main>
+      <Footer />
     </div>
   );
 }
