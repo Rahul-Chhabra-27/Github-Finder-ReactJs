cf872b1f0a0f7f2768272148aafadb1d751967f4
Rahul Chhabra <chhabrarahul2707@gmail.com>
2022-12-06 17:41:34 +0530

    All Pages are tested and working fine.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/ui/navbar';
 import Footer from './components/ui/footer';
+import Home from './pages/home';
+import About from './pages/about';
+import NotFound from './pages/not-found';
 
 function App() {
   return (
     <div className="flex flex-col justify-between h-screen">
       <Navbar />
-      <main className='container mx-auto px-3 pb-12'>Content</main>
+      <main className='container mx-auto px-3 pb-12'>
+        <Routes>
+          <Route path ='/' element={<Home />}></Route>
+          <Route path ='/about' element={<About />}></Route>
+          <Route path ='/not-found' element={<NotFound />}></Route>
+          <Route path ='/*' element={<NotFound />}></Route>
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
