d79a7ade26d8bba324419db61a5770b0894b7773
Rahul Chhabra <chhabrarahul2707@gmail.com>
2023-05-15 23:04:33 +0530

    Bug Fixed

--- src/components/users/user-search.jsx
+++ src/components/users/user-search.jsx
@@ -26,7 +26,7 @@ const UserSearch = () => {
               <input
                 type='text'
                 className='w-full pr-40 bg-gray-200 input input-lg text-black'
-                placeholder='Search'
+                placeholder='Enter Your Github Username'
                 value={text}
                 onChange={(e) => setText(e.target.value)}
               />
